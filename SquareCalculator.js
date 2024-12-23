ed4cceaadcf196a79e3e8384af5c19bba62aff47
manju <manjumm454545@gmail.com>
2024-12-23 15:59:40 +0530

    Add files via upload

---
+++ SquareCalculator.js
@@ -0,0 +1,30 @@
+import React, { useState, useMemo } from 'react';
+
+const SquareCalculator = () => {
+  const [number, setNumber] = useState(0);
+
+  
+  const squaredNumber = useMemo(() => {
+    console.log('Recalculating square...');
+    return number ** 2;
+  }, [number]);
+
+  const handleInputChange = (event) => {
+    setNumber(Number(event.target.value));
+  };
+
+  return (
+    <div>
+      <h2>Square Calculator</h2>
+      <input
+        type="number"
+        value={number}
+        onChange={handleInputChange}
+        placeholder="Enter a number"
+      />
+      <p>The square of {number} is: {squaredNumber}</p>
+    </div>
+  );
+};
+
+export default SquareCalculator;
